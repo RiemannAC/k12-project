30c6a7e412db657dd7c1033f4a8cd448c4a2f43f
Riemann <dr.mactt@gmail.com>
2018-09-08 20:19:47 +0800

    Add routes - root, calendar#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # Singular Resoruce
+  root "calendar#show"
+  resource :calendar, only: [:show], controller: :calendar
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
