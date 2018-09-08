2c56ba44dbb346435ce88822fe83ae3d8ae6a518
Riemann <dr.mactt@gmail.com>
2018-09-08 15:18:55 +0800

    Add admin and token setting to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_create :generate_authentication_token
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
+  end
+
 end
