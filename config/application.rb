d1cc5dcd636028ac4711e01d10c393e337494e49
Riemann <dr.mactt@gmail.com>
2018-09-08 20:17:06 +0800

    Add config/application.rb setting

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,24 @@
+# Calendar
+require File.expand_path('../boot', __FILE__)
+
 require_relative 'boot'
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+
+# Calendar
+Bundler.require(:default, Rails.env)
 
 module K12Project
   class Application < Rails::Application
+    # Calendar
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
