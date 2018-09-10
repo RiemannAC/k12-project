8bdc7af31c73587b440a4c142c4863071451493a
xs1523c <xs1523c@gmail.com>
2018-09-11 01:17:47 +0800

    set route&controller&build view for material

---
+++ app/controllers/materials_controller.rb
@@ -0,0 +1,5 @@
+class MaterialsController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resource :calendar, only: [:show], controller: :calendar
 
   resources :plans
+  resources :materials
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
