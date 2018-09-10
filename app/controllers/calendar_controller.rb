5bbb4de158a67c88618834a4e39eae1a88d41238
xs1523c <xs1523c@gmail.com>
2018-09-11 02:28:17 +0800

    user can add a teaching-materia file including input category_of_topic & file name by clicking add teaching-material

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,7 @@ class CalendarController < ApplicationController
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @plans = Plan.order(created_at: :desc)
+    @materials =Material.order(created_at: :desc)
   end
 
 end
