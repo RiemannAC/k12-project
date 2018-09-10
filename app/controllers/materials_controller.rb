1385e0578ef814504e901f0f36451c5c04f2b164
xs1523c <xs1523c@gmail.com>
2018-09-11 02:28:17 +0800

    user can add a teaching-materia file including input category_of_topic & file name by clicking add teaching-material

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,4 +2,29 @@ class MaterialsController < ApplicationController
   def index
     
   end
+
+  def new
+    @material = Material.new
+  end
+
+  def create
+    @material = Material.new(material_params)
+    if @material.save
+      flash[:notice] = "teaching-material file was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "teaching-material file was failed to create"
+      render :new
+    end
+  end
+
+  def show
+    @material = Material.find(params[:id])
+  end
+
+  private
+
+  def material_params
+    params.require(:material).permit(:title,:topic_id)
+  end
 end
