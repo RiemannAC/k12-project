27c6005c332a70ce886e25c8bed858a1a3e94c36
xs1523c <xs1523c@gmail.com>
2018-09-11 01:15:00 +0800

    set route&controller&build view for plan

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root "calendar#show"
   resource :calendar, only: [:show], controller: :calendar
 
+  resources :plans
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
