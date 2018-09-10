5f861a4a2ed71d2c179a0c1aa6434c61d31f3471
xs1523c <xs1523c@gmail.com>
2018-09-11 02:14:01 +0800

    user can add a teaching-plan file including input category_of_topic & file name by clicking add teaching-plan

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,4 +2,29 @@ class PlansController < ApplicationController
   def index
     
   end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      flash[:notice] = "teaching-plan file was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "teaching-plan file was failed to create"
+      render :new
+    end
+  end
+
+  def show
+    @plan = Plan.find(params[:id])
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:title,:topic_id)
+  end
 end
