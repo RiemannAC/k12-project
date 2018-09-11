8c729f3da51e2b8fe0eaefda02579e71816c53d8
Jessie Ling <jealic@gmail.com>
2018-09-11 22:18:54 +0800

    week_sche layouts version 01

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
+  
   def week_sche
-    
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @user = User.find(params[:id])
   end
+
 end
