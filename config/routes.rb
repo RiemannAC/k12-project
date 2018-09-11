225235d0e795304fc96a49c4f834ba7560827da8
Jessie Ling <jealic@gmail.com>
2018-09-11 18:45:34 +0800

    rails g conroller users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root "calendar#show"
   resource :calendar, only: [:show], controller: :calendar
 
-  devise_for :users
+  devise_for :users do
+    member do
+      get :week_sche
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
