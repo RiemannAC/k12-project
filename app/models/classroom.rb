fa1ad3bc057a666f565423485e520f670b4326ab
xs1523c <xs1523c@gmail.com>
2018-09-13 13:23:00 +0800

    set relation to classroom&teach_schedule model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,2 +1,9 @@
 class Classroom < ApplicationRecord
+  validate_presence_of :name
+  validates :name, length: { maximum: 8, message: "classroom can be allowed at most 8 words" }
+
+  belongs_to :topic
+
+  has_many :teachingschedules,dependent: :destroy 
 end
+
--- app/models/teachingschedule.rb
+++ app/models/teachingschedule.rb
@@ -1,2 +1,6 @@
 class Teachingschedule < ApplicationRecord
+  validates :theme, length: { maximum: 10, message: "theme can be allowed at most 10 words" }
+  validates :target, length: { maximum: 20, message: "target can be allowed at most 20 words" }
+
+  belongs_to :classroom
 end
