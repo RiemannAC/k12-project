f90000872abd7a880495134e44cfcaf8fa42ecde
xs1523c <xs1523c@gmail.com>
2018-09-13 14:45:17 +0800

    add sidebar to classroom_page

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,10 @@
-class ClassroomsController < ApplicationController
+class ClassroomsController < ApplicationController 
   def show
     @classroom = Classroom.find(params[:id])
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @plans = Plan.order(created_at: :desc)
+    @materials =Material.order(created_at: :desc)
+    @topics = Topic.order(created_at: :desc)
   end
 
   def new
