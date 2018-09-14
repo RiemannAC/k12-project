afc74956e7402b1d14c141276807f1c3a20e98ca
xs1523c <xs1523c@gmail.com>
2018-09-14 20:40:53 +0800

    initial_user can add teach_progress under each classroom

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -3,6 +3,7 @@ class ClassroomsController < ApplicationController
     @classroom = Classroom.find(params[:id])
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @topics = Topic.order(created_at: :desc)
+    @teachingschedule = Teachingschedule.new
   end 
 
   def new
--- app/controllers/teachingschedules_controller.rb
+++ app/controllers/teachingschedules_controller.rb
@@ -1,2 +1,21 @@
 class TeachingschedulesController < ApplicationController
+  def create
+    @classroom= Classroom.find(params[:classroom_id])
+    @teachingschedule = @classroom.teachingschedules.new(teachingschedule_params)
+
+    respond_to do |format|
+      if @teachingschedule.save
+        format.html{redirect_to classroom_url(@teachingschedule.classroom),notice:"成功新增一個教學計劃" }
+        format.json{render :show, status: :created, location: @teachingschedule}
+      else
+        format.html{render:new}
+        format.json{render json: @teachingschedule.errors,ststus: :unprocessable_entity}
+      end
+    end 
+  end
+
+  private
+  def teachingschedule_params
+    params.require(:teachingschedule).permit(:theme,:classroom_id,:feedback)
+  end
 end
