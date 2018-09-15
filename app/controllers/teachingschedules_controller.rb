85cfdfd1dadb50207e9713d0823967ff4fb0f464
xs1523c <xs1523c@gmail.com>
2018-09-15 08:58:43 +0800

    nested_duplicated_ajax_add_teaching_progress_under_classroom

--- app/controllers/teachingschedules_controller.rb
+++ app/controllers/teachingschedules_controller.rb
@@ -5,13 +5,39 @@ class TeachingschedulesController < ApplicationController
 
     respond_to do |format|
       if @teachingschedule.save
-        format.html{redirect_to classroom_url(@teachingschedule.classroom),notice:"成功新增一個教學計劃" }
-        format.json{render :show, status: :created, location: @teachingschedule}
+        #format.html{redirect_to classroom_url(@teachingschedule.classroom),notice:"成功新增一個教學計劃" }
+        #format.json{render :show, status: :created, location: @teachingschedule}
+        format.js
       else
-        format.html{render:new}
-        format.json{render json: @teachingschedule.errors,ststus: :unprocessable_entity}
+        format.html{render :new}
+        format.json{render json: @teachingschedule.errors,status: :unprocessable_entity}
+        format.js
       end
     end 
+  end
+
+  def update
+    @classroom= Classroom.find(params[:classroom_id])
+    @teachingschedule = @classroom.teachingschedules.find(params{:id})
+    respond_to do |format|
+      if @teachingschedule.update(teachingschedule_params)
+        format.html{redirect_to classroom_url(@teachingschedule.classroom),notice:"成功更新教學計劃" }
+        format.json{render :show, status: :ok, location: @teachingschedule}   
+      else
+        format.html{render :edit}
+        format.json{render json: @teachingschedule.errors,status: :unprocessable_entity} 
+      end
+    end
+  end
+
+  def destroy
+    @classroom= Classroom.find(params[:classroom_id])
+    @teachingschedule = @classroom.teachingschedules.find(params[:id])
+    @teachingschedule.destroy
+    respond_to do |format|
+      format.html{redirect_to classroom_url(@teachingschedule.classroom),notice:"成功刪除一個教學計劃" }
+      format.json{head :no_content}  
+    end
   end
 
   private
