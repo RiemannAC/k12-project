a3ae7d68121af851debeba75d1125d4abfb39e02
jealic <jealic@gmail.com>
2018-09-16 09:21:02 +0800

    rails db:migrate model project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916005951) do
+ActiveRecord::Schema.define(version: 20180916011808) do
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "start_week"
+    t.datetime "end_week"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
