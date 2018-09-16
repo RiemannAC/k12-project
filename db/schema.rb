c845ddc1d9286ab723f2294e34571021f239573e
jealic <jealic@gmail.com>
2018-09-16 09:27:24 +0800

    rails db:migrate model plan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916012309) do
+ActiveRecord::Schema.define(version: 20180916012530) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string "plan_folder_name"
+    t.integer "subject_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.datetime "start_week"
