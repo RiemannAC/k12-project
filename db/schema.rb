b28728a1a6a75f20d736bb24e20d2d21b6e9b1ed
jealic <jealic@gmail.com>
2018-09-16 09:32:19 +0800

    rails db:migrate model material

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916012530) do
+ActiveRecord::Schema.define(version: 20180916012735) do
+
+  create_table "materials", force: :cascade do |t|
+    t.string "mtrial_folder_name"
+    t.integer "subject_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plans", force: :cascade do |t|
     t.string "plan_folder_name"
