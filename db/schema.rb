dc815b973c2dceceeffce90aaffd826ccf7a0976
jealic <jealic@gmail.com>
2018-09-16 09:25:17 +0800

    rails db:migrate model subject

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916011808) do
+ActiveRecord::Schema.define(version: 20180916012309) do
 
   create_table "projects", force: :cascade do |t|
     t.datetime "start_week"
     t.datetime "end_week"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.string "classroom"
+    t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
