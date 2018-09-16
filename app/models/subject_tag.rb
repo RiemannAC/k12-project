651e450e51d34c6e403bc363dd1365773b505149
xs1523c <xs1523c@gmail.com>
2018-09-17 03:51:04 +0800

    create sunject_tag_model

---
+++ app/models/subject_tag.rb
@@ -0,0 +1,2 @@
+class SubjectTag < ApplicationRecord
+end
---
+++ test/models/subject_tag_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SubjectTagTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
