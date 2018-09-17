46d5e1cbc68666611ff65125effa99066348833c
xs1523c <xs1523c@gmail.com>
2018-09-17 08:43:52 +0800

    user_can_create_material_folder

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,12 +1,29 @@
 class MaterialsController < ApplicationController
-
+  def index
+    
+  end
   def show
-    @material = Material.find(params[:id])
-    @subject_tags = SubjectTag.order(created_at: :desc)
+    
   end
 
   def new
-    @subjects = SubjectTag.order(created_at: :desc)
-    @subject_tags = SubjectTag.order(created_at: :desc)
+    @material = Material.new 
+  end
+
+  def create
+    @material = Material.new(material_params)
+    if @material.save
+      flash[:notice] = "teaching-material file was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "teaching-material file was failed to create"
+      render :new
+    end   
+  end 
+
+  private
+
+  def material_params
+    params.require(:material).permit(:mtrial_folder_name,:subject_tag_id)
   end
 end
