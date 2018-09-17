9ccf2995b8335c809200e665f7cb7df09aa6ec69
xs1523c <xs1523c@gmail.com>
2018-09-17 22:18:23 +0800

    user_can_create+edit+destroy_Tfile_in_Tmaterial_folder

--- app/controllers/teachingfiles_controller.rb
+++ app/controllers/teachingfiles_controller.rb
@@ -1,34 +1,28 @@
 class TeachingfilesController < ApplicationController
   def create
     # upon clicking on create, determine what param id is passed
-    if params[:material_id]
-      # if it is a material id, set instance of post id as @parent
-      @parent = Material.find(params[:material_id])
-    elsif params[:plan_id]
+          # if it is a material id, set instance of post id as @parent
+      @material = Material.find(params[:material_id])
       # if it is a plan id, set instance of topic id as @parent
-      @parent = Plan.find(params[:plan_id])
-    end
 
-    @teachingfile = @parent.teachingfiles.new(teachingfile_params)
+
+    @teachingfile = @material.teachingfiles.new(teachingfile_params)
     
 
     # save teachingfile to database
-    if @teachingfile.save
+     @teachingfile.save
       # direction of save through if and elsif
       # Redirection depends on the comment's parent.
       # .is_a? method determines if it is of a certain class.  Here, is @parent
       # of class Post?  Is @parents is the same parent id passed through params?
-      if @parent.is_a?(Material) # template error with this included: (== params[:post_id])
+
+# template error with this included: (== params[:post_id])
         flash[:notice] = '成功新增教材的檔案 #{@teachingfile.filename}'
         redirect_to material_path(@teachingfile.material)
       # if not part of the class Post, is it a Topic?  If so, save here and
       # redirect to the topic after save
-      elsif @parent.is_a?(Plan)
-        flash[:notice] = '成功新增教案的檔案 #{@teachingfile.filename}'
-        redirect_to plan_path(@teachingfile.plan)
       
-      end
-    end
+    
   end
 
   def edit
@@ -53,7 +47,7 @@ class TeachingfilesController < ApplicationController
     @teachingfile = @parent.teachingfiles.find(params[:id])
 
     if @teachingfile.update(teachingfile_params)
-      flash[:notice] = "teachingfile #{@teachingfile.filename} was successfully updated"
+      flash[:notice] = "teachingfile #{@teachingfile.name} was successfully updated"
       if params[:material_id]
         redirect_to material_path(@teachingfile.material)
       elsif params[:plan_id]
