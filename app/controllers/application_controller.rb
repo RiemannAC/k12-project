44bb1ce506c76e59cd1feed22c56023f9007a36a
jealic <jealic@gmail.com>
2018-09-22 00:20:01 +0800

    set methods call for sidebar on applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :call_subject_tags
+  before_action :call_methods_for_sidebar
   
 
   private
 
-  def call_subject_tags
-     @subject_tags = SubjectTag.order(name: :desc)
+  def call_methods_for_sidebar
+    @subject_tags = SubjectTag.order(name: :desc)
+    @user = User.find_by(id: params[:user_id])
+    @materials = @user.materials.order(created_at: :desc)
+    @materials_count = @user.materials.count
+    @plans = @user.plans.order(created_at: :desc)
+    @plans_count = @user.plans.count
   end
 
   protected
