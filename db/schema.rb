cf73f182ed18c2517e4aada5e237b881ffa47f52
xs1523c <xs1523c@gmail.com>
2018-09-21 17:41:52 +0800

    reset_material_new_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920081644) do
+ActiveRecord::Schema.define(version: 20180921091007) do
 
   create_table "lessons", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180920081644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "subject_tag_id"
+    t.integer "user_id"
   end
 
   create_table "plans", force: :cascade do |t|
