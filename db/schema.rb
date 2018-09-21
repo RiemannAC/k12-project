63b7bd1dafa2ff4ac719bd85699d01f4cdd032a3
jealic <jealic@gmail.com>
2018-09-21 18:10:37 +0800

    add user_id column to plan table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180921091007) do
+ActiveRecord::Schema.define(version: 20180921100926) do
 
   create_table "lessons", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180921091007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "subject_tag_id"
+    t.integer "user_id"
   end
 
   create_table "projects", force: :cascade do |t|
