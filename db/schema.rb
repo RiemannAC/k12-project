da400f4fe797a3733d668a22ba03cb53022cd5af
xs1523c <xs1523c@gmail.com>
2018-09-22 19:47:38 +0800

    add_topic_id_in_aim_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180922111333) do
+ActiveRecord::Schema.define(version: 20180922114630) do
 
   create_table "aims", force: :cascade do |t|
     t.string "name"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "topic_id"
   end
 
   create_table "lessons", force: :cascade do |t|
