cd493279c87304cb013cc4323db23634e58a7790
Jessie Ling <jealic@gmail.com>
2018-09-23 02:30:03 +0800

    add create action in subject controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,6 +8,17 @@ class SubjectsController < ApplicationController
     @subject = @user.subjects.new
   end
 
+  def create
+    @subject = @user.subjects.new(subject_params)
+    @user = current_user
+    if @subject.save
+      redirect_to user_subjects_path(@user)
+      flash[:notice] = "班級新建成功 :)"
+    else
+      flash.now[:alert] = "新建失敗，再試一次！"
+      render :new
+    end
+  end
 
 
   private
