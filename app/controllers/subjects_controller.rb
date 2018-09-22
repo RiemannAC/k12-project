ad3f7ad9010c5fd2f7b58c2158c32cd4cd50de63
Jessie Ling <jealic@gmail.com>
2018-09-23 02:17:12 +0800

    add new action in subject controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,3 +1,18 @@
 class SubjectsController < ApplicationController
 
+  def index
+    
+  end
+
+  def new
+    @subject = @user.subjects.new
+  end
+
+
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :classroom, :grade)    
+  end
 end
