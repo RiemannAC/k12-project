6616c88d1051057c0e780cdae46d0525e0dde364
xs1523c <xs1523c@gmail.com>
2018-09-23 11:12:02 +0800

    set_user_edit_profile_css_style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
     unless @user==current_user
       redirect_to user_path(@user)
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="個人資料成功被編輯"
+      redirect_to user_path(@user)
+    else
+      flash[:alert]="未編輯成功，名字不能為空白"
+      render :edit
+    end
+  end
+ 
+  private
+  def user_params
+    params.require(:user).permit(:avatar,:name,:intro,:email,:job_title,:website)
+  end 
 
 end
