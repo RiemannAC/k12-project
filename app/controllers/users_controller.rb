b50960ea2c1049264999a104ccab3b1ddffe40d3
xs1523c <xs1523c@gmail.com>
2018-09-23 10:12:05 +0800

    create_user_edit_page_using_nav_tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    def edit
-      unless @user==current_user
-        redirect_to user_path(@user)
-      end
+    
+  end
+
+  def edit
+    unless @user==current_user
+      redirect_to user_path(@user)
     end
   end
 
