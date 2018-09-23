73cb49f172d34e414b9469879dcff3c151700562
Jessie Ling <jealic@gmail.com>
2018-09-23 19:53:09 +0800

    update the branch according to the last commit of subject_wcalendar

--- db/schema.rb
+++ db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20180924065635) do
     t.integer "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "subject_tag_id"
     t.integer "user_id"
+    t.integer "subject_tag_id"
   end
 
   create_table "plans", force: :cascade do |t|
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180924065635) do
     t.integer "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "未分類"
     t.string "grade", default: ""
     t.string "classroom", default: ""
     t.integer "students", default: 0
@@ -79,7 +80,6 @@ ActiveRecord::Schema.define(version: 20180924065635) do
     t.string "period", default: "weekly"
     t.boolean "all_day", default: false
     t.integer "user_id"
-    t.string "name"
   end
 
   create_table "teachingfiles", force: :cascade do |t|
