176e67cabc1b444e4d93ed218a106b4361941409
xs1523c <xs1523c@gmail.com>
2018-09-24 20:52:20 +0800

    set_validates_in_topic_model

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,12 +5,12 @@ class TopicsController < ApplicationController
     @topic = @subject.topics.create(topic_params)
 
     respond_to do |format|
-      if @topic.save!
-        format.html{redirect_to root_path,notice:"成功新增教學主題" }
+      if @topic.save
+        format.html{redirect_to user_subject_url(@user,@subject),notice:"成功新增教學主題" }
         format.json{render :show, status: :created, location: @topic}
         #format.js
       else
-        format.html{render :new}
+        format.html{redirect_to user_subject_url(@user,@subject),alert:"新增失敗，請完全填妥教學計劃表格資訊"}
         format.json{render json: @topic.errors,status: :unprocessable_entity}
         #format.js
       end
