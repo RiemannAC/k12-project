2dc4bc363ceaed08337e4b57096c18788e089c9e
xs1523c <xs1523c@gmail.com>
2018-09-24 21:45:51 +0800

    update+destroy_topic_in_subject_show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,6 +17,35 @@ class TopicsController < ApplicationController
     end 
   end
 
+  def update
+    @user = current_user
+    @subject = @user.subjects.find(params[:subject_id])
+    @topic = @subject.topics.find(params[:id])
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html{redirect_to user_subject_url(@user,@subject),notice:"成功更新教學計劃" }
+        format.json{render :show, status: :ok, location: @topic}  
+        #format.js 
+      else
+        format.html{redirect_to user_subject_url(@user,@subject),alert:"更新失敗，請完全填妥教學計劃表格資訊"}
+        format.json{render json: @topic.errors,status: :unprocessable_entity} 
+        #format.js
+      end
+    end
+  end
+
+  def destroy
+    @user = current_user
+    @subject = @user.subjects.find(params[:subject_id])
+    @topic = @subject.topics.find(params[:id])
+    @topic.destroy
+    respond_to do |format|
+      format.html{redirect_to user_subject_url(@user,@subject),notice:"成功刪除一個教學計劃" }
+      format.json{head :no_content}  
+      
+    end
+  end
+
   private
   def topic_params
     params.require(:topic).permit(:name,:subject_id,:start_time,:end_time,:feedback)
