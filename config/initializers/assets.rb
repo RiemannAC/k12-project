0d355333508578343b092faa5862f856e34d1031
xs1523c <xs1523c@gmail.com>
2018-10-03 11:04:59 +0800

    move_javascript_fullcalendar_as_todo_to_lesson#index

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,15 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+#這裡的每一個tag後面不能加逗號“，”
+Rails.application.config.assets.precompile += %w( js/vendor/jquery.min.js
+js/vendor/moment.min.js
+js/vendor/bootstrap.min.js 
+js/vendor/fullcalendar.js 
+js/events.js 
+js/calendar.js 
+calendar/calendar-style.css 
+calendar/fullcalendar.css 
+calendar/bootstrap.min.css 
+admin.js admin.css  )
