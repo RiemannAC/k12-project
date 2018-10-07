a099b404f99286ab3e98054fc85c4999258e3493
xs1523c <xs1523c@gmail.com>
2018-10-08 00:01:49 +0800

    set_model & set_ route_for_event

---
+++ db/migrate/20181007155802_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.datetime :start
+      t.datetime :end
+      t.integer :user_id
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007083554) do
+ActiveRecord::Schema.define(version: 20181007155802) do
 
   create_table "aims", force: :cascade do |t|
     t.string "name"
