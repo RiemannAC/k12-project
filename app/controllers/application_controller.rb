939100e2851aab0aec031950fc11831ea8695d54
xs1523c <xs1523c@gmail.com>
2018-10-08 01:02:26 +0800

    finish_event_full_calendar_todo_operation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :call_methods_on_sidebar
   
-
+  skip_before_action :verify_authenticity_token
+  
   private
 
   def call_methods_on_sidebar
