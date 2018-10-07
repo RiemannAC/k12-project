3829db53c12257febd38f4a30b9aaeb5a9e5f3f5
Jessie Ling <jealic@gmail.com>
2018-10-07 20:12:19 +0800

    migrated a join table for topics and files, and set association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181007114724) do
+ActiveRecord::Schema.define(version: 20181007120234) do
+
+  create_table "addfiles", force: :cascade do |t|
+    t.integer "topic_id"
+    t.integer "teachingfile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "aims", force: :cascade do |t|
     t.string "name"
