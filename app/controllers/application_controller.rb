6cdfb54839619e38d4560837fe641ce570a0b7a3
Jessie Ling <jealic@gmail.com>
2018-10-07 22:19:00 +0800

    set up relation among topic, teachingfile, addfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :call_methods_on_navbar
+  #before_action :call_methods_on_navbar
   
 
   private
 
   def call_methods_on_navbar
     if signed_in?
-      @lessons = Lesson.all
-      @lesson = Lesson.find_by_id(params[:lesson_id])
-    # 注意：不用路由 params 也可將 id 傳入，但括號內不可使用"實例變數"。此方法可避免巢狀路由過於複雜。
-      if params[:lesson_id]
-      topic = Topic.find_by_id(lesson.topic_id)
-      @subjects = topic.subjects.all
-      end
+      @user = current_user
+
+      # _to_do_list
+      @day = Date.today
+      @todos = Lesson.where(event_type: "todo")
     end
   end
 
