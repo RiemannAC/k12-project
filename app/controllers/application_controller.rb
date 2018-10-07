73eaba7cf1a3b3d92f29ef253eb907cc8fa27b04
Jessie Ling <jealic@gmail.com>
2018-10-07 20:43:23 +0800

    秀 班級名稱失敗，另外，不應該有班級首頁。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-  before_action :call_methods_on_sidebar
+  before_action :call_methods_on_navbar
   
 
   private
 
-  def call_methods_on_sidebar
+  def call_methods_on_navbar
     if signed_in?
-      @user = current_user
-      @subjects = @user.subjects.all
-      @materials = @user.materials.all
-      @plans = @user.plans.all
-      @subject_tags = SubjectTag.order(name: :desc)
+      @lessons = Lesson.all
+      @lesson = Lesson.find_by_id(params[:lesson_id])
+    # 注意：不用路由 params 也可將 id 傳入，但括號內不可使用"實例變數"。此方法可避免巢狀路由過於複雜。
+      if params[:lesson_id]
+      topic = Topic.find_by_id(lesson.topic_id)
+      @subjects = topic.subjects.all
+      end
     end
   end
 
