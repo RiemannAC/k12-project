8146adfbf40b8b5ee36cc31b6dc34d0adeb8ee37
xs1523c <xs1523c@gmail.com>
2018-10-11 00:14:26 +0800

    add_column_classroom.id_in_topic_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009082113) do
+ActiveRecord::Schema.define(version: 20181010161314) do
 
   create_table "addfiles", force: :cascade do |t|
     t.integer "topic_id"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20181009082113) do
     t.datetime "updated_at", null: false
     t.datetime "start_time"
     t.datetime "end_time"
+    t.integer "classroom_id"
   end
 
   create_table "users", force: :cascade do |t|
