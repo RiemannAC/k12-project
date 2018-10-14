52ca0c91ebd2ad859a6bab2a0035add1e928918c
xs1523c <xs1523c@gmail.com>
2018-10-15 00:09:33 +0800

    todo-edit+delete-popup&code_grid

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,14 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( js/vendor/jquery.min.js
-js/vendor/moment.min.js
-js/vendor/bootstrap.min.js 
-js/vendor/fullcalendar.js 
-js/events.js 
-js/calendar.js 
-js/index.js
-calendar/calendar-style.css 
-calendar/fullcalendar.css 
-calendar/bootstrap.min.css 
-admin.js admin.css  )
+# Rails.application.config.assets.precompile += %w( )
