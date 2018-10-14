829c1d7e8dbe2ed7f77644d6af75a96b5bb418a9
xs1523c <xs1523c@gmail.com>
2018-10-15 00:09:33 +0800

    todo-edit+delete-popup&code_grid

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -16,8 +16,8 @@ class LessonsController < ApplicationController
 
     # _to_do_list
     @day = Date.today
-    @todos = @user.events.all
 
+    @todos = @user.events.all
     @todo = @user.events.new
   end
  
